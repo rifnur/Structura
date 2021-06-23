a8d8145dad16b710851450584035706273756117
rifnur <rifnur69v@mail.ru>
2021-06-23 23:09:51 +0300

    Урок 7

---
+++ Lesson 7/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "Рифнур")
+                                                   String name, Model model) {model.addAttribute("name", name);
+        return "hello";
+    }
+}
